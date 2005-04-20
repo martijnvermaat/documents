d1eb6989d858890858e6bcfd8c76b3367015e354
Martijn Vermaat <martijn@vermaat.name>
2005-04-20 22:14:58 +0000

    Opgaven toegevoegd.

--- vu/iti/functioneel-programmeren/practicum-2/practicum-2-1.ml
+++ vu/iti/functioneel-programmeren/practicum-2/practicum-2-1.ml
@@ -11,4 +11,48 @@ let rec churchnumeral n =
 let backtonatural n =
   n (fun x -> x + 1) 0;;
 
+(*
+(* 'probeer, probeer' *)
 backtonatural (churchnumeral 5);;
+*)
+
+
+(* Opdracht 2 *)
+
+let equality c1 c2 = (backtonatural c1) = (backtonatural c2)
+
+
+(* Opdracht 3 *)
+
+let successor x s z = s (x s z)
+
+
+(* Opdracht 4 *)
+
+let successor' x s z = x s (s z)
+
+
+(* Opdracht 5 *)
+
+let test1 f n = backtonatural (f (churchnumeral n))
+
+(* In Haskell:
+     test1 f = backtonatural . f . churchnumeral
+   Maar ik ken geen functie compositie operator in
+   OCaml. *)
+
+
+(* Opdracht 6 *)
+
+let addition x y s z = x s (y s z);;
+
+let multiplication x y s = x (y s);;
+
+let exponentation x y = y x;;
+
+
+(* Opdracht 7 *)
+
+let test2 f n m = backtonatural (f
+                                   (churchnumeral n)
+                                   (churchnumeral m))
